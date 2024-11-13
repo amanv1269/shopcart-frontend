786b099cccfd72a636f945fa0478f4d5151011ec
amanv1269 <168532444+amanv1269@users.noreply.github.com>
2024-11-13 11:56:38 +0530

    Update ProductDetails.jsx

--- src/customer/components/ProductDetails/ProductDetails.jsx
+++ src/customer/components/ProductDetails/ProductDetails.jsx
@@ -170,11 +170,11 @@ export default function ProductDetails() {
             <div className="flex flex-wrap space-x-5 justify-center">
               {product.images.map((items) => (
                 <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg max-w-[5rem] max-h-[5rem] mt-5">
-                  <img
-                    alt={items.alt}
-                    src={product.imageUrl}
-                    className="h-full w-full object-cover object-center"
-                  />
+                  {/*   <img
+                   alt={items.alt}
+                   src={product.imageUrl}
+                  className="h-full w-full object-cover object-center"
+                 >  */}
                 </div>
               ))}
             </div>
